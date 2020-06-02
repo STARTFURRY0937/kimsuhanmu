6e5c79d37fe11d2eb897b3644265ec5421fa7a49
STARTFURRY0937 <66253600+STARTFURRY0937@users.noreply.github.com>
2020-06-02 09:35:41 +0900

    Add files via upload

---
+++ kimsuhanmu.py
@@ -0,0 +1,46 @@
+import discord
+import openpyxl
+import random
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(client.user.id)
+    print("ready")
+    game = discord.Game("김수한무")
+    await client.change_presence(status=discord.Status.online)
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("-김수한무야"):
+        await message.channel.send("무슨일 이십니까?")
+
+    if message.content.startswith("-김수한무 도움말"):
+        await message.channel.send("무슨일 이십니까?")
+
+    if message.content.startswith(""):
+        file = openpyxl.load_workbook("레벨.xlsx")
+        sheet = file.active
+        exp = [50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000]
+        i = 1
+        while True:
+            if sheet["A" + str(i)].value == str(message.author.id):
+                sheet["B" + str(i)].value = sheet["B" + str(i)].value + 2
+                if sheet["B" + str(i)].value >= exp[sheet["C" + str(i)].value - 1]:
+                    sheet["C" + str(i)].value = sheet["C" + str(i)].value + 1
+                    await message.channel.send("레벨이 올랐습니다.\n현재 레벨 "  + str(sheet["C" + str(i)].value) + "\n경험치 : " + str(sheet["B" + str(i)].value))
+                file.save("레벨.xlsx")
+                break
+
+            if sheet["A" + str(i)].value == None:
+                sheet["A" + str(i)].value = str(message.author.id)
+                sheet["B" + str(i)].value = 0
+                sheet["C" + str(i)].value = 1
+                file.save("레벨.xlsx")
+                break
+
+            i += 1
+
+
+client.run("NzE2OTkyMzcwOTgxMjA4MDc0.XtT10Q.JRt64fTreodh0SEz841h_D-ACwM")
