3194cd6b7a83a200860a4f3c16c25f8c32938ff3
STARTFURRY0937 <66253600+STARTFURRY0937@users.noreply.github.com>
2020-06-02 12:09:46 +0900

    Update kimsuhanmu.py

--- kimsuhanmu.py
+++ kimsuhanmu.py
@@ -22,6 +22,9 @@ async def on_message(message):
 
     if message.content.startswith("-김수한무 도움말"):
         await message.channel.send("도움말을 열겠습니다.")
+        
+    if message.content.startswith("-김수한무 ㅋㅋㅋ"):
+        await message.channel.send("하하하하하 :)")
 
     if message.content.startswith(""):
         file = openpyxl.load_workbook("레벨.xlsx")
